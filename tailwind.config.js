43fa8f9c73d17f00468895d86a1cf3b5f75ab3ca
willow-swamp <yusuke.y19970103@outlook.com>
2023-09-21 23:50:35 +0900

    update:トップページのレイアウト

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,22 @@ module.exports = {
   ],
   plugins : [
     require('daisyui'),
-  ]
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#34D399",   // やや明るめの緑
+          "secondary": "#60A5FA", // 明るい青
+          "accent": "#93329E",    // ワインカラーの紫
+          "neutral": "#9CA3AF",   // 中間のグレイ
+          "base-100": "#F3F4F6",  // 軽やかな背景色
+          "info": "#3B82F6",      // インフォメーション用の青
+          "success": "#10B981",   // 成功の緑
+          "warning": "#F59E0B",   // 警告のオレンジ
+          "error": "#EF4444",     // エラーの赤
+        },
+      },
+    ],
+  },
 }
