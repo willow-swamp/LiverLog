a3f018ad71f8cfe1f0bb1a8803f0b2f68fc98e8c
willow-swamp <yusuke.y19970103@outlook.com>
2023-10-02 01:36:29 +0900

    Update:初回ログイン後の処理

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class ProfilesController < ApplicationController
+class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :authentications, dependent: :destroy
   accepts_nested_attributes_for :authentications
+
+  validates :username, presence: true
 end
