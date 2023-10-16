de3d5fbee11916cd512724a84f01a0024b0115cc
willow-swamp <yusuke.y19970103@outlook.com>
2023-10-17 00:46:05 +0900

    Add:DrinkRecordsモデルの追加

--- app/javascript/controllers/record_form.js
+++ app/javascript/controllers/record_form.js
@@ -2,8 +2,8 @@ function buttonClick() {
   var btnHide = document.getElementById("hide");
   var drinkDetails = document.getElementById("drink-details");
   if (btnHide.checked) {
-    drinkDetails.display = "none";
+    drinkDetails.style.display = "none";
   } else {
     drinkDetails.style.display = "";
-  }
-}
+  };
+};
