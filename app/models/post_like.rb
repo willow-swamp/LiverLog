b199d4e53b1e2e880cd7ae62922a72ff5e43f2fa
willow-swamp <yusuke.y19970103@outlook.com>
2023-12-12 22:29:02 +0900

    Fix:デプロイのための修正

--- app/models/post_like.rb
+++ app/models/post_like.rb
@@ -1,6 +1,6 @@
 class PostLike < ApplicationRecord
   belongs_to :user
-  belongs_to :group
+  belongs_to :post
 
-  validates :user_id, uniqueness: { scope: :group_id }
+  validates :user_id, uniqueness: { scope: :post_id }
 end
